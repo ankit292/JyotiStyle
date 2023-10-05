f37c878b27319402520f3ebcf78922263b39606d
ayush <aryaaayush292@gmail.com>
2023-10-05 21:53:54 +0530

    new last night commit

--- src/App.js
+++ src/App.js
@@ -42,12 +42,55 @@ function App() {
   }
   console.log(rmvDblct2(arr))
 //last night update 9 40
+// for loop
+
+for(let a = 1; a <= 5 ;a++){
+  for(let b = 1; b <= a; b++){
+    document.write(b)
+  }
+  document.write('<br>')
+}
+
+for(let a = 1; a <= 5;a++){
+  let bc = a ;
+  for(let b = 1; b <= bc;b++){
+    document.write(a);
+  }
+  document.write('<br>')
+}
+for(let a = 5; a>= 1; a--){
+  let bc = a;
+  for(let b = 1; b <= bc; b++){
+    document.write(bc);
+  }
+  document.write('<br>')
+}
+for(let a = 5; a>=1;a--){
+  for(let b = a; b >=1;b--){
+    document.write(b);
+  }
+  document.write('<br>')
+}
+for(let a = 5;a>=1; a--){
+  for(let b = a;b<=5;b++){
+    document.write(a);
+  }
+  document.write('<br>')
+}
+for(let a = 5;a>=1;a--){
+  for(let b = 1;b<=a;b++){
+    document.write(b);
+  }
+  document.write('<br>')
+}
+
 
   return (
     <>
     <span id='demo'></span>
      <div>Ankit {name}</div>
      <button onClick={abc}>Click alert</button>
+     {/* <span id=''>{cnd1}</span> */}
      
     </>
   );
