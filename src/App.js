d1dbc7e489ef886d0399f38801841742595a58f0
ayush <aryaaayush292@gmail.com>
2023-10-18 19:20:00 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -84,9 +84,144 @@ for(let a = 5;a>=1;a--){
   document.write('<br>')
 }
 
+let Day;
+ var date = new Date();
+ var days = date.getDay()
+ console.log(date.getDate())
+
+ switch (days) {
+  case 0:  Day = "Sunday";
+    break;
+    case 1:  Day = "Monday";
+    break;
+    case 2:  Day = "Tuesday";
+    break;
+    case 3:  Day = "Wednesday";
+    break;
+    case 4:  Day = "Thursday";
+    break;
+    case 5:  Day = "Friday";
+    break;
+    case 6:  Day = "Saturday";
+    break;
+ 
+  default:
+    break;
+ }
+document.write(Day);
+// ternary operator
+let xv = 5;
+let zv;
+(xv > 10)? zv="true": zv="false";
+console.log(zv);
+// var prmt = confirm("this is Promt");
+// alert(prmt)
+// prmt? alert(true):alert(false)
+document.write('<br>')
+var tblData = [
+  ['Ankit',28,'UI Developer','3 Years','3.5 LPA'],
+  ['Rishabh',26,'Frontend Developer','3 Years','7 LPA'],
+  ['Hemant',24,'UI/UX Designer', '2 Years','5 LPA']
+]
+// map method
+var mapa = tblData.map((item,index)=>{
+  return index + ' ' + item + '<br>';
+}) 
+document.write(mapa);
+
+for(var aData = 0; aData < tblData.length;aData++){
+  console.log(tblData)
+}
+//filter 
+ var arrData = [18,23,12,14,20];
+ var arrB = arrData.filter(chck);
+ function chck(age){
+  return age >= 18;
+ }
+ console.log(arrB);
+ //forEach
+ var feach = [18,23,12,14,20];
+ feach.forEach(loop);
+ function loop(value,index){
+  document.write(index + ' : ' + value + '<br>');
+ }
+ //object
+ var obj1 = {
+  fname: 'Ankit',
+  lname: 'Kumar',
+  age: 28,
+  fullName: function(){
+   return this.fname + ' ' + this.lname
+  }
+ }
+ console.log(obj1.fullName());
+ //for in loop
+ var obj2 = {
+  fname: 'ankit',
+  lname: 'kumar',
+  age: 25,
+  city: 'delhi'
+ }
+ for(var key1 in obj2){
+  document.write(obj2[key1] + '<br>');
+ }
+//map method
+var mapb = [
+  {
+    name: 'Ankit',
+    age: 25,
+    city: 'delhi',
+    
+  },
+  {
+    name: 'rishabh',
+    age: 25,
+    city: 'gaziyabad'
+  }
+];
+var mapc = mapb.map((item,index)=>{
+  return item.name ;
+});
+document.write(mapc)
+// function mapFunc(x){
+//   return x.name + x.age + x.city;
+// }
+
+//math method
+// var matharr = [13,23,45,56,32]
+var mathmax = Math.max(13,23,45,56,32);
+console.log(mathmax);
+var mathmin = Math.min(13,23,45,56,32);
+console.log(mathmin);
+// date method
+var dt = new Date();
+var todaydt = dt.toDateString();
+console.log(todaydt);
+var birthdt = dt.getDate() + '/' + (dt.getMonth()+1) + '/' + dt.getFullYear();
+
+console.log(birthdt )
+
+
+
 
   return (
     <>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Position</th>
+          <th>Exp.</th>
+          <th>CTC</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td> </td>
+        </tr>
+      </tbody>
+    </table>
     <span id='demo'></span>
      <div>Ankit {name}</div>
      <button onClick={abc}>Click alert</button>
